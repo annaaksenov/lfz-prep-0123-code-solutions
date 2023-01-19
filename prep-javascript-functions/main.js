e2306bf009dc8d9bc749f843ffb8428163f17d9b
Anna Aksenov <annaaksenov8@gmail.com>
2023-01-19 02:49:54 +0000

    Completed prep-javascript-functions.

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,56 @@
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+var addTwoNumbersResult = addTwoNumbers(2, 2);
+console.log('addTwoNumbers Exercise', addTwoNumbersResult);
+
+function convertHoursToMinutes(hours) {
+  return hours;
+}
+
+var Minutes = convertHoursToMinutes(120);
+console.log('convertHoursToMinutes Exercise', Minutes);
+
+function getGreeting(name) {
+  return 'Hi ' + name;
+}
+
+var sayHey = getGreeting('Sam');
+console.log('getGreeting Exercise', sayHey);
+
+function addAndMultiplyBy5(num1, num2) {
+  return (num1 + num2) * 5;
+}
+
+var sumOfThat = addAndMultiplyBy5(10, 5);
+console.log('addAndMultiplyBy5 Exercise', sumOfThat);
+
+function multiplyAndDivideby5(num1, num2) {
+  return (num1 * num2 / 5);
+}
+var getMyAnswer = multiplyAndDivideby5(35, 10);
+console.log('multiplyAndDivideby5 Exercise', getMyAnswer);
+
+function subtractTwoNumbers(num1, num2) {
+  return num1 - num2;
+}
+var subThat = subtractTwoNumbers(22, 7);
+console.log('subtractTwoNumbers Exercise', subThat);
+
+function getCircleCircumference(radius) {
+  return radius * 3.14 * 2;
+}
+var circleThat = getCircleCircumference(5);
+console.log('getCircleCircumference', circleThat);
+
+function getFullName(firstName, lastName) {
+  return firstName + lastName;
+}
+var getWho = getFullName('Juan', ' Ramirez');
+console.log('getFullName Exercise', getWho);
+
+function cube(number) {
+  return number * number * number;
+}
+var sq3 = cube(5);
+console.log('cube Exercise', sq3);
