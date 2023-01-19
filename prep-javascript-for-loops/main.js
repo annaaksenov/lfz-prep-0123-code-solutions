70c73b228e2cf21c9fec4d8060bd587828076edf
Anna Aksenov <annaaksenov8@gmail.com>
2023-01-19 23:11:56 +0000

    Completed prep-javascript-for-loops(1).

--- prep-javascript-for-loops/main.js
+++ prep-javascript-for-loops/main.js
@@ -1,3 +1,9 @@
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+for (let i = 0; i < 20; i += 2) {
+  console.log(i);
+}
 const str = 'Time till explosion: ';
 for (let i = 100; i > 0; i--) {
   console.log(str + i);
